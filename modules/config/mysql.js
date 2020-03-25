bdf2fe306f988ef0d48d5dbaba24e9c30bcc02af
xunzhaoshitouyu <617703167@qq.com>
2020-03-25 23:01:18 +0800

    修改gitignore文件

--- modules/config/mysql.js
+++ modules/config/mysql.js
@@ -1,12 +1,12 @@
 // sequelize构造函数传递的配置项
 mysql = {
-  host: 'xxxx',
-  user: 'xxxx',
-  password: 'xxxx',
-  port: '3306',
-  database: 'xxxx',
-  charset: 'UTF8_GENERAL_CI', // 连接字符集
-  dialect: 'mysql',
+  host: "xxxx",
+  user: "xxxx",
+  password: "xxxx",
+  port: "3306",
+  database: "xxxx",
+  charset: "UTF8_GENERAL_CI", // 连接字符集
+  dialect: "mysql",
   pool: {
     max: 5,
     min: 0,

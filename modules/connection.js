1378a3e6ba67941ce473f3d420899d32e5f1e103
xunzhaoshitouyu <617703167@qq.com>
2020-03-29 22:17:43 +0800

    添加chalk美化控制台显示内容

--- modules/connection.js
+++ modules/connection.js
@@ -1,5 +1,7 @@
-const Sequelize = require('sequelize');
-const mysqlConfig = require('./config/mysql');
+const Sequelize = require("sequelize");
+const mysqlConfig = require("./config/mysql");
+const chalk = require("chalk");
+const log = console.log;
 
 const sequelize = new Sequelize(
   mysqlConfig.database,
@@ -8,7 +10,7 @@ const sequelize = new Sequelize(
   {
     host: mysqlConfig.host,
     port: mysqlConfig.port,
-    dialect: 'mysql',
+    dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
@@ -19,12 +21,12 @@ const sequelize = new Sequelize(
 );
 
 // 测试链接
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    log(chalk.green("Connection has been established successfully."));
+  })
+  .catch(err => {
+    log(chalk.red("Unable to connect to the database:" + err));
+  });
 module.exports = sequelize;

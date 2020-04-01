c378189a4f24f7c4f6cf01f663d9368378208241
xunzhaoshitouyu <617703167@qq.com>
2020-04-01 22:18:05 +0800

    config

--- modules/config/mysql.js
+++ modules/config/mysql.js
@@ -1,12 +1,12 @@
 // sequelize构造函数传递的配置项
 mysql = {
-  host: '172.30.1.19',
-  user: 'Samuel',
-  password: '1qaz@WSX',
-  port: '3306',
-  database: 'monitor',
-  charset: 'UTF8_GENERAL_CI', // 连接字符集
-  dialect: 'mysql',
+  host: "xxxx",
+  user: "xxxx",
+  password: "xxxx",
+  port: "3306",
+  database: "xxxx",
+  charset: "UTF8_GENERAL_CI", // 连接字符集
+  dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
